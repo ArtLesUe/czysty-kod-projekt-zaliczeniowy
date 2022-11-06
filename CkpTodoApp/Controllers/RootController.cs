09400cae96074b97cb905793ca083ce16c9ac426
Artur Leśnik <artur.lesnik@edu.uekat.pl>
2022-11-06 09:33:33 +0100

    dodano: przykładowy endpoint główny do aplikacji

---
+++ CkpTodoApp/Controllers/RootController.cs
@@ -0,0 +1,27 @@
+﻿using CkpTodoApp.Responses;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace CkpTodoApp.Controllers
+{
+  [Route("/")]
+  [ApiController]
+  public class RootController : ControllerBase
+  {
+    private readonly ILogger<RootController> _logger;
+
+    public RootController(ILogger<RootController> logger)
+    {
+      _logger = logger;
+    }
+
+    [HttpGet]
+    public RootResponse Get()
+    {
+      return new RootResponse { 
+        Status = "ok" 
+      };
+    }
+  }
+}

253226baf406dc2fc40b2294ec0ce1f1067b1131
Artur Leśnik <artur.lesnik@edu.uekat.pl>
2022-11-11 09:54:36 +0100

    dodano: zaimplementowano klasę i kontroler użytkownika

--- CkpTodoApp/Interfaces/ApiUserInterface.cs
+++ CkpTodoApp/Interfaces/ApiUserInterface.cs
@@ -1,6 +1,15 @@
 ﻿namespace CkpTodoApp.Interfaces
 {
-    public interface ApiUserInterface
-    {
-    }
+  public interface ApiUserInterface
+  {
+    int Id { get; set; }
+    
+    string Name { get; set; }
+
+    string Surname { get; set; }
+
+    string Email { get; set; }
+
+    string PasswordHashed { get; set; }
+  }
 }
--- CkpTodoApp/Models/ApiUserModel.cs
+++ CkpTodoApp/Models/ApiUserModel.cs
@@ -1,6 +1,26 @@
-﻿namespace CkpTodoApp.Models
+﻿using CkpTodoApp.Interfaces;
+
+namespace CkpTodoApp.Models
 {
-    public class ApiUserModel
+  public class ApiUserModel : ApiUserInterface
+  {
+    public ApiUserModel(int id, string name, string surname, string email, string passwordHashed) 
     {
+      Id = id;
+      Name = name;
+      Surname = surname;
+      Email = email;
+      PasswordHashed = passwordHashed;
     }
+
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public string Surname { get; set; }
+
+    public string Email { get; set; }
+
+    public string PasswordHashed { get; set; }
+  }
 }

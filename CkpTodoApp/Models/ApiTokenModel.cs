788301339a4776bb52af58d3c0f6849cc9a08eb8
Artur Leśnik <artur.lesnik@edu.uekat.pl>
2022-11-11 12:02:33 +0100

    dodano: model do przechowywania tokenów logowania

---
+++ CkpTodoApp/Models/ApiTokenModel.cs
@@ -0,0 +1,20 @@
+﻿using CkpTodoApp.Interfaces;
+
+namespace CkpTodoApp.Models
+{
+  public class ApiTokenModel : IApiTokenInterface 
+  {
+    public ApiTokenModel(int id, int userId, string token)
+    {
+      Id = id;
+      UserId = userId;
+      Token = token;
+    }
+
+    public int Id { get; set; }
+
+    public int UserId { get; set; }
+
+    public string Token { get; set; }
+  }
+}

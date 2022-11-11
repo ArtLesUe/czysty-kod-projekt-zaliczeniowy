2750c647727053fe2b22ec0248534116a191259f
Artur Leśnik <artur.lesnik@edu.uekat.pl>
2022-11-11 14:27:35 +0100

    dodano: autentyfikację użytkownika w bazie danych

--- CkpTodoApp/Requests/UserLoginRequest.cs
+++ CkpTodoApp/Requests/UserLoginRequest.cs
@@ -1,4 +1,9 @@
 ﻿using CkpTodoApp.DatabaseControllers;
+using System.Security.Cryptography;
+using System.Text;
+using System.Collections.Generic;
+using System.Text.Json;
+using CkpTodoApp.Models;
 
 namespace CkpTodoApp.Requests
 {
@@ -16,5 +21,34 @@ namespace CkpTodoApp.Requests
         Password.Length > 0;
     }
 
+    public int Authenticate()
+    {
+      MD5 hasher = MD5.Create();
+      byte[] inputBytes = Encoding.ASCII.GetBytes(Password);
+      byte[] hashBytes = hasher.ComputeHash(inputBytes);
+
+      DatabaseManagerController databaseManagerController = new DatabaseManagerController();
+      String resultSql = databaseManagerController.ExecuteSQLQuery(
+        @"SELECT json_group_array( 
+          json_object(
+            'Id', Id,
+            'Name', Name,
+            'Surname', Surname,
+            'Email', Email,
+            'PasswordHashed', PasswordHashed
+          )
+        )
+        FROM users
+        WHERE Email = '" + Login + @"' AND PasswordHashed = '" + 
+        Convert.ToHexString(hashBytes) + @"';"
+      );
+
+      var userList = JsonSerializer.Deserialize<List<ApiUserModel>>(resultSql);
+      if (userList == null) { return 0; }
+      if (userList.Count == 0) { return 0; }
+      int userId = userList[0].Id;
+
+      return userId;
+    }
   }
 }

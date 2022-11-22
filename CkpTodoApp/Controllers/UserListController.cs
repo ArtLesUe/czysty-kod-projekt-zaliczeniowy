da03bd8199a78e21a0a83fd56075b23bf8e235b1
Artur Leśnik <artur.lesnik@edu.uekat.pl>
2022-11-22 18:20:04 +0100

    dodano: pusty kontroler odpowiadający za zwracanie listy użytkowników

---
+++ CkpTodoApp/Controllers/UserListController.cs
@@ -0,0 +1,29 @@
+﻿using CkpTodoApp.Responses;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CkpTodoApp.Controllers
+{
+  [Route("api/user/list")]
+  [ApiController]
+  public class UserListController : ControllerBase
+  {
+    private readonly ILogger<UserLoginTokenController> _logger;
+
+    public UserListController(ILogger<UserLoginTokenController> logger)
+    {
+      _logger = logger;
+    }
+
+    [DisableCors]
+    [HttpGet]
+    public RootResponse Get()
+    {
+      return new RootResponse
+      {
+        Status = "ok"
+      };
+    }
+  }
+}

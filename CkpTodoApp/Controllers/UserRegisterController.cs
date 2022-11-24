b348b633689212ccd8b8c5ee23cbb9a8cd440b03
Artur Leśnik <artur.lesnik@edu.uekat.pl>
2022-11-24 13:57:14 +0100

    dodano: utworzono nowy endpoint do rejestracji użytkowników

---
+++ CkpTodoApp/Controllers/UserRegisterController.cs
@@ -0,0 +1,26 @@
+﻿using CkpTodoApp.Responses;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CkpTodoApp.Controllers
+{
+  [Route("api/user/register")]
+  [ApiController]
+  public class UserRegisterController : ControllerBase
+  {
+    private readonly ILogger<UserRegisterController> _logger;
+
+    public UserRegisterController(ILogger<UserRegisterController> logger)
+    {
+      _logger = logger;
+    }
+
+    [DisableCors]
+    [HttpPost]
+    public RootResponse Post()
+    {
+      return new RootResponse { Status = "OK" };
+    }
+  }
+}

fb75d7a122549fded993c994ad5cf536a17c4384
Artur Leśnik <artur.lesnik@edu.uekat.pl>
2022-11-25 09:45:55 +0100

    dodano: nowy endpoint odpowiadający za usuwanie użytkowników

---
+++ CkpTodoApp/Controllers/UserDeleteController.cs
@@ -0,0 +1,26 @@
+﻿using CkpTodoApp.Responses;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Cors;
+
+namespace CkpTodoApp.Controllers
+{
+  [Route("api/user/delete/{id}")]
+  [ApiController]
+  public class UserDeleteController : ControllerBase
+  {
+    private readonly ILogger<UserDeleteController> _logger;
+
+    public UserDeleteController(ILogger<UserDeleteController> logger)
+    {
+      _logger = logger;
+    }
+
+    [DisableCors]
+    [HttpGet]
+    public RootResponse Get(int id)
+    {
+      return new RootResponse { Status = "deleted" };
+    }
+  }
+}

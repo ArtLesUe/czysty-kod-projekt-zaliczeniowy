98cf591f85d0d501ee04a08d2fb0c8e9a5d0e0b5
Tomasz Janus <tomajan160@student.polsl.pl>
2022-11-27 04:12:43 +0100

    dodano: utworzenie endpointu umożliwiającego edytowanie danych profilowych użytkownika

---
+++ CkpTodoApp/Requests/UserEditRequest.cs
@@ -0,0 +1,16 @@
+namespace CkpTodoApp.Requests;
+
+public class UserEditRequest
+{
+    public string? Name { get; set; }
+
+    public string? Surname { get; set; }
+
+    public string? AboutMe { get; set; }
+
+    public string? City { get; set; }
+
+    public string? Country { get; set; }
+
+    public string? University { get; set; }
+}

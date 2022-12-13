5ca2253f6ee93e3337c2743d2fe9994863c983ea
Weronika Ładak <weronika.ladak1@gmail.com>
2022-12-13 21:40:32 +0100

    zastsowano AuthService w Controllers/Event

--- CkpTodoApp/Controllers/Event/AddEventController.cs
+++ CkpTodoApp/Controllers/Event/AddEventController.cs
@@ -1,39 +1,25 @@
 using System.Globalization;
-using CkpTodoApp.Models.ApiToken;
 using CkpTodoApp.Models.Event;
 using CkpTodoApp.Requests;
 using CkpTodoApp.Responses;
-using CkpTodoApp.Services.ApiTokenService;
+using CkpTodoApp.Services.AuthService;
 using CkpTodoApp.Services.Event;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Primitives;
 
 namespace CkpTodoApp.Controllers.Event;
 
 [Route("api/events/add")]
 [ApiController]
-public class AddEventController : ControllerBase
+public class AddEventController : AuthService
 {
   [HttpPost]
   public RootResponse Post(EventRequest eventRequest)
-  {
-    Request.Headers.TryGetValue("token", out StringValues headerValues);
-    var jsonWebToken = headerValues.FirstOrDefault();
-
-    if (string.IsNullOrEmpty(jsonWebToken))
+  {  
+    var rootResponse = CheckAuth();
+    
+    if (rootResponse.Status != "OK")
     {
-      Response.StatusCode = 401;
-      return new RootResponse { Status = "auth-failed" };
-    }
-
-    var apiToken = new ApiTokenModel(0, 0, jsonWebToken);
-    var apiTokenService = new ApiTokenService();
-    apiTokenService.Verify(apiToken);
-
-    if (apiToken.UserId == 0)
-    {
-      Response.StatusCode = 401;
-      return new RootResponse { Status = "auth-failed" };
+      return rootResponse;
     }
         
     var newEvent = new EventModel(
--- CkpTodoApp/Controllers/Event/EditEventController.cs
+++ CkpTodoApp/Controllers/Event/EditEventController.cs
@@ -1,37 +1,23 @@
-using CkpTodoApp.Models.ApiToken;
 using CkpTodoApp.Requests;
 using CkpTodoApp.Responses;
-using CkpTodoApp.Services.ApiTokenService;
+using CkpTodoApp.Services.AuthService;
 using CkpTodoApp.Services.Event;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Primitives;
 
 namespace CkpTodoApp.Controllers.Event;
 
 [Route("api/events/edit/{id:int}")]
 [ApiController]
-public class EditEventController : ControllerBase
+public class EditEventController : AuthService
 {
     [HttpPost]
     public RootResponse Post(EventRequest eventRequest, int id)
     {
-        Request.Headers.TryGetValue("token", out StringValues headerValues);
-        var jsonWebToken = headerValues.FirstOrDefault();
-
-        if (string.IsNullOrEmpty(jsonWebToken))
+        var rootResponse = CheckAuth();
+    
+        if (rootResponse.Status != "OK")
         {
-            Response.StatusCode = 401;
-            return new RootResponse { Status = "auth-failed" };
-        }
-
-        var apiToken = new ApiTokenModel(0, 0, jsonWebToken);
-        var apiTokenService = new ApiTokenService();
-        apiTokenService.Verify(apiToken);
-        
-        if (apiToken.UserId == 0)
-        {
-            Response.StatusCode = 401;
-            return new RootResponse { Status = "auth-failed" };
+            return rootResponse;
         }
 
         var eventService = new EventService();

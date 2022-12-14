c1114647a62cf60816a5729e5ade1d7644706860
Artur Leśnik <artur.lesnik@edu.uekat.pl>
2022-12-14 09:50:38 +0100

    naprawiono: usunięto regresję związaną ze zwracanymi kodami tekstowymi statusów

--- CkpTodoApp/Commons/StatusCodeEnum.cs
+++ CkpTodoApp/Commons/StatusCodeEnum.cs
@@ -1,15 +1,15 @@
 namespace CkpTodoApp.Commons;
 
-public enum StatusCodeEnum
+public struct StatusCodeEnum
 {
-    Ok,
-    AuthFailed,
-    WrongData,
-    Deleted,
-    Checked,
-    DeletingNotExistingForbidden,
-    CheckingNotExistingForbidden,
-    SelfDeletionForbidden,
-    UserDoesNotExist,
-    EmptyPasswordNotPermitted
+    public const string Ok = "OK";
+    public const string AuthFailed = "auth-failed";
+    public const string WrongData = "wrong-data";
+    public const string Deleted = "deleted";
+    public const string Checked = "checked";
+    public const string DeletingNotExistingForbidden = "deleting-not-existing-forbidden";
+    public const string CheckingNotExistingForbidden = "checking-not-existing-forbidden";
+    public const string SelfDeletionForbidden = "self-deletion-forbidden";
+    public const string UserDoesNotExist = "user-not-exists";
+    public const string EmptyPasswordNotPermitted = "empty-password-not-permitted";
 }
